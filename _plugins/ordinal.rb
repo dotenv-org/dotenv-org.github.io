7541de59d2c75c1d68f7c4b2feed871fd23a2315
Scott Motte <mot@mot.la>
2023-10-19 10:45:26 -0700

    New no nonsense blog page

---
+++ _plugins/ordinal.rb
@@ -0,0 +1,32 @@
+# https://github.com/patrickcate/Jekyll-Ordinal
+# Public: Print the ordinal of a Jekyll date.
+#
+# date - The Jekyll date String being passed.
+# day  - The date variable formatted to the day of the month, without padded 0's.
+#
+# Examples
+#
+#   {{ page.date | ordinal }}
+#   # => "st"
+#
+# Returns the ordinal String.
+
+module Jekyll
+  module Ordinal
+    def ordinal(datetime)
+      day = datetime.strftime("%-d");
+      case day
+      when "1", "21", "31";
+        "st"
+      when "2", "22";
+        "nd"
+      when "3", "23";
+        "rd"
+      else
+        "th"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::Ordinal)
--- blog/index.md
+++ blog/index.md
@@ -1,7 +1,18 @@
 ---
-layout: default
+layout: blog2
 title: "Blog"
 ---
 
-{% include blog/hero.html %}
-{% include blog/list.html %}
+{% for post in site.categories.blog %}
+  {% assign currentdate = post.date | date: "%Y" %}
+  {% if currentdate != date %}
+    {% unless forloop.first %}</ul>{% endunless %}
+<h3 id="y{{post.date | date: "%Y"}}">{{ currentdate }}</h3>
+<ul class="entrylist">
+    {% assign date = currentdate %}
+  {% endif %}
+    <li>
+      <a href="{{ post.url }}">{{ post.title }}</a> — {{ post.date | date: "%B %-d" }}
+    </li>
+  {% if forloop.last %}</ul>{% endif %}
+{% endfor %}
